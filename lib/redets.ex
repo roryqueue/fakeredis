c2361bef70d8181743769ec1ca33f294fd74bc23
Rory Quinlan <rjamesquinlan@gmail.com>
2016-12-14 21:41:51 +0700

    implement get and getset

--- lib/redets.ex
+++ lib/redets.ex
@@ -25,6 +25,8 @@ defmodule Redets do
       "SETNX" -> setnx(conn, command_args)
       "SETEX" -> setex(conn, command_args)
       "PSETEX" -> psetex(conn, command_args)
+      "GET" -> get(conn, command_args)
+      "GETSET" -> get(conn, command_args)
       _ -> raise ArgumentError, "Can't match command"
     end
   end
@@ -33,6 +35,8 @@ defmodule Redets do
   def setnx!(conn, command_args), do: command!(conn, ["SETNX" | command_args])
   def setex!(conn, command_args), do: command!(conn, ["SETEX" | command_args])
   def psetex!(conn, command_args), do: command!(conn, ["PSETEX" | command_args])
+  def get!(conn, command_args), do: command!(conn, ["GET" | command_args])
+  def getset!(conn, command_args), do: command!(conn, ["GETSET" | command_args])
 
   def command!(conn, command) do
     case command(conn, command) do
@@ -113,5 +117,26 @@ defmodule Redets do
 
   def setex(conn, command_args), do: set_with_exp(conn, command_args, "EX")
   def psetex(conn, command_args), do: set_with_exp(conn, command_args, "PX")
+
+  # get only has one argument outside the reference to our redets instance (the key)
+  # so we'll allow a one-element list for consistency but also the key itself
+  def get(conn, [key | _tail]), do: get(conn, key)
+
+  def get(conn, key) do
+    value_list = :ets.lookup(conn, key)
+    if value_list === [] do
+      nil
+    else
+      [value | _tail] = value_list
+      value
+    end
+  end
+
+
+  def getset(conn, command_args) do
+    return_val = get(conn, command_args)
+    set(conn, command_args)
+    return_val
+  end
 
 end
